cf78c6f1678a4c2f93657ee72fc632287af177e0
ganmor <morgan.laupies@gmail.com>
2012-06-26 14:48:59 +0200

    Multiple bugfix & cleanup, now works...

--- backbone_filters.js
+++ backbone_filters.js
@@ -5,38 +5,33 @@
 			if (_(filters).isEmpty()) 
 				return true;
 			
-			var def = new $.Defered();
-			var beforeFiltersDefArray = new $.Deferred();
+			var def = new $.Deferred();
+			var beforeFiltersDefArray = [];
 			_(filters).each(function(func, filterRoute) {
 				if (!_.isRegExp(filterRoute)) {
 					filterRoute = new RegExp(filterRoute);
 				}
 				if (filterRoute.test(fragment)) {
-					var result = (_.isFunction(func) ? func.apply(this, args) : this[func].apply(this, args));
-					beforeFiltersDefArray.push( result ); 
+					var regexpResult = new RegExp( filterRoute ).exec( fragment );
+					var result = (_.isFunction(func) ? func.apply(this, [ args , regexpResult ]) : this[func].apply(this, [ args , regexpResult] ));
+					beforeFiltersDefArray.push( result );
 				}
 			}, this );
 			
 			$.when.apply( null, beforeFiltersDefArray ).done( function(){
 				def.resolve();
 			});
-			// TODO: If any of them failed, cancel this
 			return def;
 		},
-		
-		chritophe : function(){
-			
-			jkfdjdfj
-		},
-		
-		route: function(route, name, callback) {
+		 
+		route: function( route , name , callback) {
 			var instance_ = this;
 			Backbone.history || (Backbone.history = new Backbone.History);
 			if (!_.isRegExp(route)) route = this._routeToRegExp(route);
 			Backbone.history.route(route, _.bind(function(fragment) {
 				var args = this._extractParameters(route, fragment);
-				this._runFilters(this.before, fragment, args)).done(function(){
-					callback.apply(instance_, args);
+				this._runFilters(this.before, fragment, args).done(function(){
+					callback && callback.apply(instance_, [ args , fragment ] );
 					instance_.trigger.apply(instance_, ['route:' + name].concat(args));
 				});
 			}, this));
